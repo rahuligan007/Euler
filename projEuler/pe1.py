6e76ac07826f2ce97e3982c8311cf4f11f4153e2
Rahul Madanahalli <rahuligan007@gmail.com>
2014-06-16 14:09:03 -0700

    1&2 success

--- projEuler/pe1.py
+++ projEuler/pe1.py
@@ -11,20 +11,17 @@ class Pe1(object): # Pe1 inherits everything from object class
         self.top = top
         self.rage = [5]*0
     def findNums(self): # finds all numbers that are multiples of 5 or 3 and stores it in rage
-        print ("Swag")
-       
-        for i in range(self.top): #goes from 0-999
+       for i in range(self.top): #goes from 0-999
             if i%5==0 or i%3 ==0:
                 self.rage.append(i)
     def addNums(self): #finds the sum of all numbers in the list and prints that
-        print ("swag")
-        sum = 0
+        total = 0
         for i in self.rage:
-            sum = sum + i
-        print (sum)
+            total = total + i
+        print (total)
     
-    if __name__=="__main__": # = public static void main
-        run = Pe1(0,1000)#you pass in a value for all parameters of the init except for self
-                #you don't do anything for self
-        run.findNums()
-        run.addNums()
+if __name__=="__main__": # = public static void main
+    run = Pe1(0,1000)#you pass in a value for all parameters of the init except for self
+              #you don't do anything for self
+    run.findNums()
+    run.addNums()
--- projEuler/pe2.py
+++ projEuler/pe2.py
@@ -1,7 +1,24 @@
-class pe2 (pe1):
+#finds the sum of all the fibonacci #'s from 1-4,000,000 that are even.
+
+from Pe1 import * # imports everything from the Pe1 file
+
+class Pe2 (Pe1):
     def __init__(self, bottom, top):
-        self.bottom = bottom
-        self.top = top
-        self.rage = [5]*0
+        super(Pe2,self).__init__(bottom, top) #calls the super constructor
+
+    def findNums(self):
+        f1 = 1
+        f2 = 2
+        while f2 < self.top: # need colon for while and if's
+            if f2%2 ==0:
+                self.rage.append(f2)
+            temp = f2
+            f2 = f1 + f2
+            f1 = temp
+
+if __name__=="__main__":
+    run = Pe2(0,4000000)
+    run.findNums()
+    run.addNums() #calls addNums in the Pe1 class
     
     
